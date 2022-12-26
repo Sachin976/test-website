25defc1bab5e826d2e3f30d0ba5047e755e836bd
sachinp2e <sachinshoff@gmail.com>
2022-12-26 12:04:25 +0530

    bug resolve

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,12 @@ import store from '../src/redux/store';
 {
   /* SCSS files */
 }
+import '../styles/globals.scss';
 import '../src/containers/landing/landing.scss';
 import '../src/components/kosha/headerOne/header.scss';
 import '../src/containers/solutionPage/solution.scss';
 import '../src/containers/aboutUs/about.scss';
 import '../styles/Home.module.scss';
-import '../styles/globals.scss';
 import '../src/components/kosha/Layout/layout.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
